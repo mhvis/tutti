59d9a09e938cd028a26e38f389fe91f1c7e4defd
Heijden <g.w.v.d.heijden@student.tue.nl>
2020-04-25 23:50:05 +0200

    cleanup

--- pennotools/src/qrekening/process.py
+++ pennotools/src/qrekening/process.py
@@ -13,6 +13,7 @@ def read_exc(wb, debet, persons):
              | persons: dict (qID:DavilexPerson)
     - Returns: list(DavilexPerson)
     """
+    current_person = None
     for s in wb.sheets():
         col_names = s.row(0)
         person_finished = True
@@ -23,7 +24,7 @@ def read_exc(wb, debet, persons):
                 col_value[name.value] = value
             if col_value['Zoekcode'] != '' and 'totaal' in col_value['Omschrijving']:
                 person_finished = True
-            elif not person_finished:
+            elif not person_finished and current_person is not None:
                 current_person.add_boekstuk(col_value, debet)
             elif col_value['Zoekcode'] != '' and col_value['Omschrijving'] != '':
                 try:
@@ -47,77 +48,82 @@ def combine_persons(davilex_people):
 
 
 def get_value(p, header):
-    if header == 'StuurStatus' or header == 'Opmerkingen':
-        return ''
-    elif header == 'CODE' or header == 'mandaatid':
-        return p.id
-    elif header == 'Naam' or header == 'naam':
-        return p.name
-    elif header == 'Email':
-        return p.get_email()
-    elif header == 'Bankreknr' or header == 'IBAN':
-        return p.get_iban()
-    elif header == 'Deb Tot Open':
-        return p.get_debet_total()
-    elif 'Deb Omschrijving' in header:
+    values = {
+        'StuurStatus': '',
+        'Opmerkingen': '',
+        'CODE': p.id,
+        'mandaatid': p.id,
+        'Naam': p.name,
+        'naam': p.name,
+        'Email': p.get_email(),
+        'Bankreknr': p.get_iban(),
+        'IBAN': p.get_iban(),
+        'Deb Tot Open': p.get_debet_total(),
+        'Deb Datum': p.get_debet_dates(),
+        'Deb Bedrag': p.get_debet_amounts(),
+        'Deb Open': p.get_debet_amounts(),
+        'Cred Tot Open': p.get_credit_total(),
+        'Cred Datum': p.get_credit_dates(),
+        'Cred Bedrag': p.get_credit_amounts(),
+        'Cred Open': p.get_credit_amounts(),
+        'Totaal Open Tekst': '{0:.2f}'.format(p.get_total()),
+        'Totaal Open Temp': p.get_total(),
+        'bedrag': p.get_total(),
+        'beschrijving': 'Qrekening %s %s' % (datetime.datetime.today().strftime('%B'),
+                                             datetime.datetime.today().strftime('%Y')),
+        'endtoendid': p.id + datetime.date.today().strftime('%m') + datetime.date.today().strftime('%y')
+    }
+    if 'Deb Omschrijving' in header:
         return p.get_debet_description()
-    elif header == 'Deb Datum':
-        return p.get_debet_dates()
-    elif header == 'Deb Bedrag' or header == 'Deb Open':
-        return p.get_debet_amounts()
-    elif header == 'Cred Tot Open':
-        return p.get_credit_total()
     elif 'Cred Omschrijving' in header:
         return p.get_credit_description()
-    elif header == 'Cred Datum':
-        return p.get_credit_dates()
-    elif header == 'Cred Bedrag' or header == 'Cred Open':
-        return p.get_credit_amounts()
-    elif header == 'Totaal Open Tekst':
-        return '{0:.2f}'.format(p.get_total())
-    elif header == 'Totaal Open Temp' or header == 'bedrag':
-        return p.get_total()
-    elif header == 'beschrijving':
-        return 'Qrekening %s %s' % (datetime.datetime.today().strftime('%B'),
-                                    datetime.datetime.today().strftime('%Y'))
-    elif header == 'endtoendid':
-        return p.id + datetime.date.today().strftime('%m') + datetime.date.today().strftime('%y')
-    return ''
+    try:
+        return values[header]
+    except KeyError:
+        return ''
 
 
 def initialize_workbook(dav_people, workbook):
-    """Write people to the excelsheet."""
+    """Initialize workbook to write people to the excelsheet."""
     creditors = workbook.add_worksheet('Crediteuren')
-    cred_row = 1
     debtors = workbook.add_worksheet('Debiteuren')
-    debt_row = 1
     debtors_self = workbook.add_worksheet('DebiteurenZelf')
-    debt_self_row = 1
     external = workbook.add_worksheet('Externen')
-    external_row = 1
     for s in [creditors, debtors, debtors_self, external]:
         for i in range(len(output_headers)):
             s.write(0, i, output_headers[i])
+    write_workbook(dav_people, creditors, debtors, debtors_self, external)
+    return workbook
+
+
+def write_row(ws, row, headers, p):
+    for i in range(len(headers)):
+        ws.write(row, i, get_value(p, headers[i]))
+    return 1
+
+
+def write_workbook(dav_people, creditors, debtors, debtors_self, external):
+    """Write people to the excelsheet."""
+    cred_row, debt_row, debt_self_row, external_row = (1, 1, 1, 1)
     for key, p in dav_people.items():
         # Is this person from Q?
         if p.qPerson is not None:
             if p.get_total() < 0:
-                for i in range(len(output_headers)):
-                    creditors.write(cred_row, i, get_value(p, output_headers[i]))
-                cred_row += 1
+                cred_row += write_row(creditors, cred_row, output_headers, p)
             elif p.get_total() >= 0 and len(p.get_iban()) < 1:
-                for i in range(len(output_headers)):
-                    debtors_self.write(debt_self_row, i, get_value(p, output_headers[i]))
-                debt_self_row += 1
+                debt_self_row += write_row(debtors_self, debt_self_row, output_headers, p)
             else:
-                for i in range(len(output_headers)):
-                    debtors.write(debt_row, i, get_value(p, output_headers[i]))
-                debt_row += 1
+                debt_row += write_row(debtors, debt_row, output_headers, p)
         else:
-            for i in range(len(output_headers)):
-                external.write(external_row, i, get_value(p, output_headers[i]))
-            external_row += 1
-    return workbook
+            external_row += write_row(external, external_row, output_headers, p)
+
+
+def write_sepa_bedrag(ws, row, value, headers, p):
+    for i in range(len(headers)):
+        if headers[i] == 'bedrag':
+            ws.write(row, i, value)
+        else:
+            ws.write(row, i, get_value(p, headers[i]))
 
 
 def write_sepa(dav_people, workbook):
@@ -128,24 +134,14 @@ def write_sepa(dav_people, workbook):
             s.write(0, i, sepa_headers[i])
     for key, p in dav_people.items():
         if p.qPerson is not None:
-            if p.get_total() < 0:
-                pass
-            elif p.get_total() >= 0 and len(p.get_iban()) < 1:
+            if p.get_total() < 0 or (p.get_total() >= 0 and len(p.get_iban()) < 1):
                 pass
             else:
                 total = p.get_total()
                 while total > 100:
-                    for i in range(len(sepa_headers)):
-                        if sepa_headers[i] == 'bedrag':
-                            debtors.write(debt_row, i, 100)
-                        else:
-                            debtors.write(debt_row, i, get_value(p, sepa_headers[i]))
+                    write_sepa_bedrag(debtors, debt_row, 100, sepa_headers, p)
                     total -= 100
                     debt_row += 1
-                for i in range(len(sepa_headers)):
-                    if sepa_headers[i] == 'bedrag':
-                        debtors.write(debt_row, i, total)
-                    else:
-                        debtors.write(debt_row, i, get_value(p, sepa_headers[i]))
+                write_sepa_bedrag(debtors, debt_row, total, sepa_headers, p)
                 debt_row += 1
     return workbook
