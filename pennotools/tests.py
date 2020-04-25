057380340c3e180e01c6ad2d28f9d6fa1aaca23d
Heijden <g.w.v.d.heijden@student.tue.nl>
2020-04-26 00:30:32 +0200

    tests

--- pennotools/tests.py
+++ pennotools/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class QrekeningTestCase(TestCase):
+    """Test cases for generation of Q rekening files."""
+    def test_qrekening(self):
+        """Test cases for generation of qrekening file."""
+        pass
+
+
+    def test_sepa(self):
+        """Test cases for generation of sepa file."""
+        pass
