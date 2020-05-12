4839be85fd9130a0d0e8943ae123b496707a5ff1
Heijden <g.w.v.d.heijden@student.tue.nl>
2020-05-12 23:37:56 +0200

    birthday requests

--- duqduqgo/urls.py
+++ duqduqgo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from duqduqgo.views import Qalendar
+from duqduqgo.views import Qalendar, Birthdays
 
-app_name = 'pennotools'
+app_name = 'duqduqgo'
 urlpatterns = [
     path('qalendar/', Qalendar.as_view(), name='qalendar'),
+    path('birthday_events', Birthdays.as_view()),
 ]
--- duqduqgo/views.py
+++ duqduqgo/views.py
@@ -1,11 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import JsonResponse
 from members.models import Person
+from datetime import datetime
 
-# Create your views here.
+
 class DuqduqgoAccessMixin(PermissionRequiredMixin):
     """Use this mixin to protect views from prying eyes."""
     permission_required = 'duqduqgo.can_access'
+
 
 class Qalendar(TemplateView):
     template_name = "duqduqgo/qalendar.html"
@@ -15,11 +18,41 @@ class Qalendar(TemplateView):
         persons = Person.objects.all()
         calendar_events = []
         for person in persons:
+            date = datetime(datetime.today().year, person.date_of_birth.month, person.date_of_birth.day).date()
             calendar_events.append({
                 "title": person.first_name,
-                "start": "2019-09-05T09:00:00",
-                "end": "2019-09-05T18:00:00"
+                "start": str(date),
+                "end": str(date)
             })
-        context['week_birthdays'] = Person.objects.all()
+        context['week_birthdays'] = calendar_events
         return context
+
+
+class Birthdays(View):
+
+    def get(self, request, *args, **kwargs):
+        r_date_start = datetime.strptime(request.GET.get('start', '')[0:10], '%Y-%m-%d')
+        r_date_end = datetime.strptime(request.GET.get('end', '')[0:10], '%Y-%m-%d')
+        print(r_date_start)
+        persons = Person.objects.filter(date_of_birth__isnull=False)
+        #persons = Person.objects.filter(date_of_birth__month__gte=r_date_start.month,
+        #                                date_of_birth__month__lte=r_date_end.month)
+        print(persons)
+        calendar_events = []
+        for person in persons:
+            date_1 = datetime(r_date_start.year, person.date_of_birth.month, person.date_of_birth.day).date()
+            date_2 = datetime(r_date_start.year+1, person.date_of_birth.month, person.date_of_birth.day).date()
+            calendar_events.append({
+                "title": person.first_name,
+                "start": str(date_1),
+                "end": str(date_1)
+            })
+            calendar_events.append({
+                "title": person.first_name,
+                "start": str(date_2),
+                "end": str(date_2)
+            })
+        return JsonResponse(calendar_events, safe=False)
+
+
 
