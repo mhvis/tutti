37f4b50a59df5fd382b52287ab2fc861a7bb4405
Maarten Visscher <mail@maartenvisscher.nl>
2020-08-03 23:27:31 +0200

    Add donors app

---
+++ donors/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from django_countries.fields import CountryField
+from localflavor.generic.models import IBANField
+
+
+class Donor(models.Model):
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(blank=True)
+    salutation = models.CharField(max_length=30,
+                                  choices=(('heer', 'Heer'), ('mevrouw', 'Mevrouw')),
+                                  blank=True,
+                                  help_text="Aanhef, om eventueel te gebruiken bij automatische e-mails.")
+
+    # Address
+    street = models.CharField(max_length=150, blank=True)
+    postal_code = models.CharField(max_length=30, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    country = CountryField(blank=True, default='NL')
+
+    iban = IBANField(blank=True, verbose_name='IBAN')
+
+    amount = models.DecimalField(max_digits=8,
+                                 decimal_places=2,
+                                 null=True,
+                                 blank=True,
+                                 help_text="Yearly donation amount.")
+
+    newsletter = models.BooleanField(null=True,
+                                     help_text="Whether the donor wants to receive the newsletter.")
