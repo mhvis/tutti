0c487ad93bd7ae3758f72e2c7a8ddedf005e6df2
jigstar <51361363+jigstar@users.noreply.github.com>
2020-08-13 12:39:36 +0200

    faqts pagina leden en commissies

--- faqts/views.py
+++ faqts/views.py
@@ -1,8 +1,28 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from members.models import QGroup, Person, Instrument
+from django.db.models import Count
 
 from django.views.generic import TemplateView
 
 
 class FaQtsView(LoginRequiredMixin, TemplateView):
     template_name = "faqts/faqts.html"
+
+    def get_context_data(self, **kwargs):
+        groups = QGroup.objects.all()
+        persons = Person.objects.all()
+        double_names = list(Person.objects.values('first_name').annotate(
+                                name_count=Count('first_name')).filter(
+                                name_count__gt=1).values_list('first_name', flat=True))
+        persongroupvalues = list(Person.objects.all().values('first_name', 'last_name', 'groups').values_list(
+                                'first_name', 'last_name','groups'))
+        instruments = Instrument.objects.all()
+
+
+        context = {'groups':groups,
+                   'persons':persons,
+                   'double_names': double_names,
+                   'instruments':instruments,
+                   'persongroupvalues':persongroupvalues}
+        return context
