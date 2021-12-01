0d6f3b0d2451672dc3a7e7035fd5cca4c26fd1d9
Maarten Visscher <mail@maartenvisscher.nl>
2021-12-01 13:56:14 +0100

    Custom 500

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from pages.views import IndexView, AboutView
 
+
+# This is a test view, can be removed
+def server_error_view(request):
+    raise RuntimeError('Test')
+
+
 app_name = 'pages'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='about'),
+    path('500/', server_error_view),
 ]
